6348fe76ec8199b7d49f20fc30876596f9dff30a
Julian <mrjrhirst@gmail.com>
2023-10-23 13:42:51 +0100

    ex1 task1

--- exercise001.js
+++ exercise001.js
@@ -1,14 +1,11 @@
 const promise = new Promise((resolve, reject) => {
-	// Generate random number between 0 and 9
-	const randomInt = Math.floor(Math.random() * 10);
-
-	if (randomInt % 2 === 0) {
-		// Success
+	if (Math.random() < 0.5) {
 		setTimeout(() => resolve('done'), 2000);
 	} else {
-		// Failure
 		setTimeout(() => reject('error'), 2000);
 	}
 });
 
-// Your solution(s) to exercise001 here!
+promise
+	.then((result) => console.log(`Yay! Promise resolved with response: ${result}`))
+	.catch((err) => console.log(`Boo. Promise rejected with response:  ${err}`));
