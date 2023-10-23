8bee3eec5d1bd077fa13e16bacb569daa4908392
Julian <mrjrhirst@gmail.com>
2023-10-23 13:55:43 +0100

    Exercise 1 Task 2

--- exercise001.js
+++ exercise001.js
@@ -1,11 +1,27 @@
-const promise = new Promise((resolve, reject) => {
-	if (Math.random() < 0.5) {
-		setTimeout(() => resolve('done'), 2000);
-	} else {
-		setTimeout(() => reject('error'), 2000);
-	}
-});
 
-promise
-	.then((result) => console.log(`Yay! Promise resolved with response: ${result}`))
-	.catch((err) => console.log(`Boo. Promise rejected with response:  ${err}`));
+// promise
+// 	.then((result) => console.log(`Yay! Promise resolved with response: ${result}`))
+// 	.catch((err) => console.log(`Boo. Promise rejected with response:  ${err}`));
+
+const main = async () => {
+
+	
+	try {
+	const result = await new Promise((resolve, reject) => {
+		if (Math.random() < 0.5) {
+			setTimeout(() => resolve('done'), 2000);
+		} else {
+			setTimeout(() => reject('error'), 2000);
+		}
+	});
+	console.log(`Yay! Promise resolved with response: ${result}`);
+} catch (err) {
+	console.log(`Boo. Promise rejected with response:  ${err}`);
+}
+
+
+
+}
+const numberOfPromises = 10;
+console.log(`Testing with ${numberOfPromises} promises`);
+for (let i=0; i<numberOfPromises; i++) main();
