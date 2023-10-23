721d5fd3cc94e6b2b5bfa98f93cc84766f9ad7e2
Julian <mrjrhirst@gmail.com>
2023-10-23 13:56:56 +0100

    Reformatted code

--- exercise001.js
+++ exercise001.js
@@ -5,23 +5,21 @@
 
 const main = async () => {
 
-	
+
 	try {
-	const result = await new Promise((resolve, reject) => {
-		if (Math.random() < 0.5) {
-			setTimeout(() => resolve('done'), 2000);
-		} else {
-			setTimeout(() => reject('error'), 2000);
-		}
-	});
-	console.log(`Yay! Promise resolved with response: ${result}`);
-} catch (err) {
-	console.log(`Boo. Promise rejected with response:  ${err}`);
-}
-
-
+		const result = await new Promise((resolve, reject) => {
+			if (Math.random() < 0.5) {
+				setTimeout(() => resolve('done'), 2000);
+			} else {
+				setTimeout(() => reject('error'), 2000);
+			}
+		});
+		console.log(`Yay! Promise resolved with response: ${result}`);
+	} catch (err) {
+		console.log(`Boo. Promise rejected with response:  ${err}`);
+	}
 
 }
 const numberOfPromises = 10;
 console.log(`Testing with ${numberOfPromises} promises`);
-for (let i=0; i<numberOfPromises; i++) main();
+for (let i = 0; i < numberOfPromises; i++) main();
